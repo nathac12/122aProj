1eafe700f7da694ed9b705c440eea462eb900b69
Nathan Chen <nathac12@uci.edu>
2025-11-22 22:31:45 +0000

    setup

---
+++ project.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import sys
+
+FUNCINDEX = 1
+
+def main():
+    args = sys.argv
+    funcName = args[FUNCINDEX]
+    
+    match funcName:
+        case "import":
+        case
+
+if __name__ == "__main__":
+    main()
