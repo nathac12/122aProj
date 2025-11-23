cb24e7c5cf80b157fbeba83e15ab62095d5b1b26
malachek <malachykennedy1@gmail.com>
2025-11-23 03:39:07 -0800

    csv reading

--- project.py
+++ project.py
@@ -1,13 +1,21 @@
 import mysql.connector
 import sys
 
+import importCSVs
+
 FUNCINDEX = 1
+USER = "root"
+PASSWORD = ""
 def initDB():
+    if PASSWORD == "" or USER == "":
+        print("set your USER and PASSWORD!")
+        return
+
     mydb = mysql.connector.connect(
         #edpost said to format it like this
         host="localhost",
-        user="test",
-        password="password"
+        user=USER,
+        password=PASSWORD
     )
     return mydb
 
@@ -18,7 +26,8 @@ def main():
     # no need to check args length
     match funcName:
         case "import":
-            return 0
+            # args([FUNCINDEX + 1]) : folderName (str)
+            return importCSVs.import_data(args[FUNCINDEX + 1])
         case "insertAgentClient":
             return 0
         case "addCustomizedModel":
@@ -36,6 +45,7 @@ def main():
         case _:
             print("wrong function name")
             return -1
+
 
 
 if __name__ == "__main__":
