cba01006e29d747f47f8548cfcf1f99bc4a5b372
nathac12 <nathac12@uci.edu>
2025-11-22 18:13:17 -0800

    problems with file dir

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import sys
-
+import os 
+import csv
 FUNCINDEX = 1
 def initDB():
     mydb = mysql.connector.connect(
@@ -11,14 +12,52 @@ def initDB():
     )
     return mydb
 
+def get_dir(targetName: str):
+    baseDir = os.path.dirname(os.path.abspath(__file__)) 
+    csvDir = os.path.join(baseDir, "test_data_project_122a")
+    for filename in os.listdir(csvDir):
+        if targetName in filename:  
+            print("Found:", filename)
+            return os.path.join(csvDir, filename)
+    print("file not found")
+    return None
+
+def parse_csv(fileDir: str, fileName: str, cursor, dataBase):
+    file = open(fileDir, newline="")
+    fileName = fileName[:-4]
+
+    reader = csv.reader(file)
+    header = next(reader)
+    columns = ", ".join(header)
+    placeholders = ", ".join(["%s"] * len(header))
+
+    sql = f"INSERT INTO {fileName} ({columns}) VALUES ({placeholders})"
+    rows = []
+    for row in reader:
+        rows.append(row) 
+
+    val = rows
+    cursor.executemany(sql, val)
+    dataBase.commit()
+
+def create_table(fileName: str, cursor):
+    #creates a table, if one already exists delete the prev one 
+    sql = f"DROP TABLE IF EXISTS {fileName}"
+    cursor.execute(sql) 
+    sql = f"CREATE TABLE {fileName}" #dont know how to do this right now
+    cursor.execute(sql) 
+
+
 def main():
     args = sys.argv
     funcName = args[FUNCINDEX]
     dataBase = initDB()
+    myCursor = dataBase.cursor()
     # no need to check args length
     match funcName:
         case "import":
-            return 0
+            fileName = args[2]
+            parse_csv(get_dir(fileName), fileName, myCursor, dataBase)
         case "insertAgentClient":
             return 0
         case "addCustomizedModel":
@@ -40,6 +79,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 
